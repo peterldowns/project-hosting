71f11f8198a532ac6e6ff5769c98e72b590d2570
Peter Downs <peterldowns@gmail.com>
2015-11-07 12:47:19 -0500

    Add phood.club config.

--- scripts/nginx_config.sh
+++ scripts/nginx_config.sh
@@ -1,7 +1,9 @@
 sudo cp ./nginx.sites/peterdowns.com /etc/nginx/sites-available/peterdowns.com 
 sudo cp ./nginx.sites/bookshrink.com /etc/nginx/sites-available/bookshrink.com 
+sudo cp ./nginx.sites/phood.club /etc/nginx/sites-available/phood.club
 sudo ln -f -s /etc/nginx/sites-available/peterdowns.com \
               /etc/nginx/sites-enabled/peterdowns.com
 sudo ln -f -s /etc/nginx/sites-available/bookshrink.com \
               /etc/nginx/sites-enabled/bookshrink.com
-
+sudo ln -f -s /etc/nginx/sites-available/phood.club\
+              /etc/nginx/sites-enabled/phood.club
