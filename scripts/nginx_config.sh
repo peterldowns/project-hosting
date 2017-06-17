65a6aba9c7b13f87c873070c5cbde04caaa6834a
Peter Downs <peterldowns@gmail.com>
2017-06-17 08:35:32 -0400

    Add freezine API.

--- scripts/nginx_config.sh
+++ scripts/nginx_config.sh
@@ -12,21 +12,3 @@ enable_site peterdowns.com
 enable_site bookshrink.com
 enable_site freezine.xyz
 enable_site catchall_redirect
-#make_available ./nginx.sites/peterdowns.com
-#make_available ./
-#
-#sudo cp ./nginx.sites/peterdowns.com /etc/nginx/sites-available/peterdowns.com 
-#sudo ln -f -s /etc/nginx/sites-available/peterdowns.com \
-#              /etc/nginx/sites-enabled/peterdowns.com
-#
-#sudo cp ./nginx.sites/bitcoin.peterdowns.com /etc/nginx/sites-available/bitcoin.peterdowns.com 
-#sudo ln -f -s /etc/nginx/sites-available/bitcoin.peterdowns.com \
-#              /etc/nginx/sites-enabled/bitcoin.peterdowns.com
-#
-#sudo cp ./nginx.sites/bookshrink.com /etc/nginx/sites-available/bookshrink.com 
-#sudo ln -f -s /etc/nginx/sites-available/bookshrink.com \
-#              /etc/nginx/sites-enabled/bookshrink.com
-#
-#sudo cp ./nginx.sites/freezine.xyz /etc/nginx/sites-available/freezine.xyz 
-#sudo ln -f -s /etc/nginx/sites-available/freezine.xyz \
-#              /etc/nginx/sites-enabled/freezine.xyz
